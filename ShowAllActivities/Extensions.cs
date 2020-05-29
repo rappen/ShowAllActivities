25ae6169f6ac6a0104f092fa3cd9b37745b3956d
Jonas Rapp <jonas@jonasr.app>
2020-05-29 23:16:01 +0200

    Some serious refactoring

--- ShowAllActivities/Extensions.cs
+++ ShowAllActivities/Extensions.cs
@@ -19,30 +19,54 @@ namespace ShowAllActivities
                 bag.TraceBlockEnd();
                 return false;
             }
-            var nullCondition = query.GetCondition("activityid", ConditionOperator.Null);
+            var nullCondition = query.GetCondition("activityid", ConditionOperator.Null, bag.TracingService);
             if (nullCondition == null)
             {
                 bag.Trace("No null condition for activityid");
                 bag.TraceBlockEnd();
                 return false;
             }
-            var regardingCondition = query.GetCondition("regardingobjectid", ConditionOperator.Equal);
-            if (regardingCondition == null || regardingCondition.Values.Count != 1 || !(regardingCondition.Values[0] is Guid))
+            Guid contactId;
+            var regardingCondition = query.GetCondition("regardingobjectid", ConditionOperator.Equal, bag.TracingService);
+            if (regardingCondition != null &&
+                regardingCondition.Operator.Equals(ConditionOperator.Equal) &&
+                regardingCondition.Values.Count == 1 &&
+                regardingCondition.Values[0] is Guid regardingConditionId)
+            {
+                contactId = regardingConditionId;
+                query.RemoveCondition(regardingCondition, bag.TracingService);
+            }
+            else
             {
                 bag.Trace("No condition for regardingobjectid");
+                var contactCondition = query.GetCondition("contactid", ConditionOperator.Equal, bag.TracingService);
+                if (contactCondition != null &&
+                    contactCondition.Operator.Equals(ConditionOperator.Equal) &&
+                    contactCondition.Values.Count == 1 &&
+                    contactCondition.Values[0] is Guid contactConditionId)
+                {
+                    contactId = contactConditionId;
+                    query.RemoveCondition(contactCondition, bag.TracingService);
+                }
+                else
+                {
+                    bag.Trace("No condition for contactid");
+                    bag.TraceBlockEnd();
+                    return false;
+                }
+            }
+            if (contactId.Equals(Guid.Empty))
+            {
+                bag.Trace("No contactid identified in query");
                 bag.TraceBlockEnd();
                 return false;
             }
-            var regardingId = (Guid)regardingCondition.Values[0];
-            bag.Trace($"Found regarding id: {regardingId}");
 
-            bag.Trace("Removing triggering conditions");
-            query.Criteria.Conditions.Remove(nullCondition);
-            query.Criteria.Conditions.Remove(regardingCondition);
+            query.RemoveCondition(nullCondition, bag.TracingService);
 
             bag.Trace("Adding link-entity and condition for activity party");
             var leActivityparty = query.AddLink("activityparty", "activityid", "activityid");
-            leActivityparty.LinkCriteria.AddCondition("partyid", ConditionOperator.Equal, regardingId);
+            leActivityparty.LinkCriteria.AddCondition("partyid", ConditionOperator.Equal, contactId);
             bag.TraceBlockEnd();
             return true;
         }
@@ -58,59 +82,6 @@ namespace ShowAllActivities
             }
             bag.TraceBlockEnd();
             return false;
-        }
-
-        private static ConditionExpression GetCondition(this QueryExpression query, string attribute, ConditionOperator? oper)
-        {
-            var result = query.Criteria?.GetCondition(attribute, oper);
-            if (result!=null)
-            {
-                return result;
-            }
-            foreach (var link in query.LinkEntities)
-            {
-                var linkresult = link.GetCondition(attribute, oper);
-                if (linkresult != null)
-                {
-                    return linkresult;
-                }
-            }
-            return null;
-        }
-
-        private static ConditionExpression GetCondition(this LinkEntity linkentity, string attribute, ConditionOperator? oper)
-        {
-            var result = linkentity.LinkCriteria?.GetCondition(attribute, oper);
-            if (result != null)
-            {
-                return result;
-            }
-            foreach (var link in linkentity.LinkEntities)
-            {
-                var linkresult = link.GetCondition(attribute, oper);
-                if (linkresult != null)
-                {
-                    return linkresult;
-                }
-            }
-            return null;
-        }
-
-        private static ConditionExpression GetCondition(this FilterExpression filter, string attribute, ConditionOperator? oper)
-        {
-            var result = filter?.Conditions?.FirstOrDefault(c => c.AttributeName.Equals(attribute) && (oper == null || oper == c.Operator));
-            if (result != null)
-            {
-                return result;
-            }
-            foreach (var subfilter in filter?.Filters)
-            {
-                if (subfilter.GetCondition(attribute, oper) is ConditionExpression subresult)
-                {
-                    return subresult;
-                }
-            }
-            return null;
         }
     }
 }
--- ShowAllActivities/ShowAllActivitiesPlugin.cs
+++ ShowAllActivities/ShowAllActivitiesPlugin.cs
@@ -3,10 +3,6 @@ using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ShowAllActivities
 {
@@ -14,41 +10,39 @@ namespace ShowAllActivities
     {
         public override void Execute(JonasPluginBag bag)
         {
-            var context = bag.PluginContext;
-            var service = bag.Service;
             try
             {
-                if (context.MessageName != "RetrieveMultiple" || context.Stage != 20 || context.Mode != 0 ||
-                    !context.InputParameters.Contains("Query"))
+                if (bag.PluginContext.MessageName != "RetrieveMultiple" || bag.PluginContext.Stage != 20 || bag.PluginContext.Mode != 0 ||
+                    !bag.PluginContext.InputParameters.Contains("Query"))
                 {
                     bag.Trace("Not expected context");
                     return;
                 }
 
-                if (context.InputParameters["Query"] is QueryExpression query)
+                if (bag.PluginContext.InputParameters["Query"] is QueryExpression query)
                 {
                     bag.trace("Checking QueryExpression");
 #if DEBUG
-                    var fetch1 = ((QueryExpressionToFetchXmlResponse)service.Execute(new QueryExpressionToFetchXmlRequest() { Query = query })).FetchXml;
+                    var fetch1 = ((QueryExpressionToFetchXmlResponse)bag.Service.Execute(new QueryExpressionToFetchXmlRequest() { Query = query })).FetchXml;
                     bag.Trace($"Query before:\n{fetch1}");
 #endif
                     if (query.ReplaceRegardingCondition(bag))
                     {
 #if DEBUG
-                        var fetch2 = ((QueryExpressionToFetchXmlResponse)service.Execute(new QueryExpressionToFetchXmlRequest() { Query = query })).FetchXml;
+                        var fetch2 = ((QueryExpressionToFetchXmlResponse)bag.Service.Execute(new QueryExpressionToFetchXmlRequest() { Query = query })).FetchXml;
                         bag.Trace($"Query after:\n{fetch2}");
 #endif
-                        context.InputParameters["Query"] = query;
+                        bag.PluginContext.InputParameters["Query"] = query;
                     }
                 }
-                else if (context.InputParameters["Query"] is FetchExpression fetch)
+                else if (bag.PluginContext.InputParameters["Query"] is FetchExpression fetch)
                 {
                     bag.trace("Checking FetchExpression");
                     bag.Trace($"Query before:\n{fetch.Query}");
                     if (fetch.ReplaceRegardingCondition(bag))
                     {
                         bag.Trace($"Query after:\n{fetch.Query}");
-                        context.InputParameters["Query"] = fetch;
+                        bag.PluginContext.InputParameters["Query"] = fetch;
                     }
                 }
             }
